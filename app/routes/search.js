d016f1a7abb8a5186d5a0ebbc5e9d6dd8f25d6cf
yiweiguo <yiweiguo0301@gmail.com>
2016-11-22 15:15:27 -0500

    add datamap with query highlight on

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,11 +1,89 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host		: 'cis550group25.c4piije6mfdf.us-east-1.rds.amazonaws.com',
+	user		: 'user',
+	password	: 'user',
+	database	: 'Abc'	
+});
+var dict = {};
+dict['United States'] = 'USA';
+dict['Canada'] = 'CAN';
+dict['Croatia'] = 'HRV';
+dict['Mexico'] = 'MEX';
+dict['Cuba'] = 'CUB';
+dict['Russia'] = 'RUS';
+dict['Bulgaria'] = 'BGR';
+dict['Argentina'] = 'ARG';
+dict['Uruguay'] = 'URY';
+dict['China'] = 'CHN';
+dict['Italy'] = 'ITA';
+dict['Spain'] = 'ESP';
+dict['France'] = 'FRA';
+dict['Lithuania'] = 'LTU';
+dict['Australia'] = 'AUS';
+dict['Brazil'] = 'BRA';
 
+// connection.connect();
+
+function query1(res){
+	connection.query('select CName from Country where Num_Gold = (select max(Num_Gold) from Country)', function(err, rows, fields){
+		if(err) throw err;
+		else{
+	
+  		output_country(res, rows, dict);
+		console.log(dict[rows[0].CName]) 
+		}
+	});
+}
+
+function query2(res){
+	connection.query('select distinct c.CName from Country c inner join Organizer o on c.CName = o.CName where c.Num_Gold = (select max(c.Num_Gold) from Country c inner join Organizer o on c.CName = o.CName)'
+		, function(err, rows, fields){
+		if(err) throw err;
+		else{
+	
+  		output_country(res, rows, dict);
+		
+		}
+	});
+}
+
+function query3(res){
+	connection.query('select c.CName, count(distinct p.AName) as num from Country c inner join Bornin b on\
+	 b.NOC_code = c.NOC_code inner join Athlete a on a.AName = b.AName inner join\
+	  Participate p on a.AName = p.AName\
+	where p.EName = "basketball"\
+	group by b.NOC_code\
+	order by num'
+	, function(err, rows, fields){
+		if(err) throw err;
+		else{
+	
+  		output_country(res, rows, dict);
+		
+		}
+	});
+}
+function output_country(res, rows, dict){
+	var short_name, i;
+	short_name = [];
+	for(i = 0; i < rows.length; i++){
+		short_name.push(dict[rows[i].CName]);
+	}
+	res.render('search', {
+  			pageTitle: 'Search',
+  			pageID: 'Search',
+  			Country: short_name
+  		});
+	// for(i = 0; i < short_name.length; i++){
+		console.log(rows.length);
+		console.log(short_name[0]);
+	// }
+}
 router.get('/search', function(req, res) {
-  res.render('search', {
-  	pageTitle: 'Search',
-  	pageID: 'Search'
-  });
+	query3(res);  
 });
 
 module.exports = router;
